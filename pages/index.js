c34ed52a942e3184a381ce7dd2cc527dee0965ba
Matt Anderson <matt.anderson@mississauga.ca>
2021-07-14 10:35:49 -0400

    created some base components

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,22 @@
-import Head from 'next/head';
-// import Image from 'next/image';
-// import styles from '../styles/Home.module.css';
-import { react as HomeContent } from '../content/home.md';
+import Layout from '../components/base/Layout';
+import { attributes } from '../content/settings.md';
 
-export default function Home() {
+export default function Home({ title, description, ...props }) {
     return (
-        <>
-            <Head>
-                <script
-                    src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-                    async
-                ></script>
-            </Head>
-            <div className="container mx-auto px-10 md:px-40 py-10">
-                <h1 className="text-4xl">Welcome to mattdev.ca</h1>
-                <small className="text-lg mt-2 block mb-4">
-                    It's a bit sparse, but it's a quick read.
-                </small>
+        <Layout pageTitle={title}>
+            <h1 className="title">Welcome to my blog!</h1>
+            <div>homepage is</div>
 
-                <section className="my-4">
-                    <HomeContent />
-                </section>
-
-                <section className="md:w-4/5">
-                    <h2 className="text-2xl mb-2">What have I done lately?</h2>
-                    <hr className="my-4" />
-                    <h3 className="text-xl mb-2">Words</h3>
-                    <article>
-                        <a
-                            href="https://mattander.github.io/words/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            I made a silly webapp where you can list words
-                        </a>
-                        . I wrote this in vue becuase I wanted to take the new{' '}
-                        <span className="font-mono text-sm">
-                            &lang;script setup&rang;
-                        </span>{' '}
-                        for a spin. It was fun. I might add more sometime.
-                        Here's the{' '}
-                        <a
-                            href="https://github.com/mattander/words"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            repo on github
-                        </a>
-                        .
-                    </article>
-                </section>
-                <footer className="pt-10">
-                    &copy; Matt Anderson 2021 - Present
-                </footer>
-            </div>
-        </>
+            <main>Posts go here!</main>
+        </Layout>
     );
 }
+
+export const getStaticProps = async (context) => {
+    // your logic
+    const siteSettings = await import('../content/settings.md');
+
+    return {
+        props: { siteSettings },
+    };
+};
