15a91a38cc248ea1065cc5bada02de7f96192a28
Matt Anderson <matt.anderson@mississauga.ca>
2021-07-23 13:56:46 -0400

    fixed the error

--- pages/index.js
+++ pages/index.js
@@ -2,40 +2,40 @@ import Container from '../components/layout/Container';
 import Layout from '../components/layout/Layout';
 import Head from 'next/head';
 import { CMS_NAME } from '../lib/constants';
-import { getAllPosts, getPostBySlug, getTopLevelPages } from '../lib/api';
+import { getAllPosts, getPageBySlug, getTopLevelPages } from '../lib/api';
 
 export default function Index({ page, nav, posts, projects }) {
-	return (
-		<>
-			<Layout title="Matt Anderson's development website" nav={nav}>
-				<Head>
-					<title>Mattdev - Home</title>
-				</Head>
-				<Container>
-					<section>{page.content}</section>
-					<section>
-						<h2>Projects</h2>
-						<p>Here's what I've been working on lately</p>
-					</section>
-				</Container>
-			</Layout>
-		</>
-	);
+    return (
+        <>
+            <Layout title="Matt Anderson's development website" nav={nav}>
+                <Head>
+                    <title>Mattdev - Home</title>
+                </Head>
+                <Container>
+                    <section>{page.content}</section>
+                    <section>
+                        <h2>Projects</h2>
+                        <p>Here's what I've been working on lately</p>
+                    </section>
+                </Container>
+            </Layout>
+        </>
+    );
 }
 
 export async function getStaticProps() {
-	const page = getPostBySlug('home', 'page', ['content', 'title']);
-	const projects = getAllPosts('project');
-	const posts = getAllPosts('post');
+    const page = getPageBySlug('home', 'page', ['content', 'title']);
+    const projects = getAllPosts('project');
+    const posts = getAllPosts('post');
 
-	const nav = getTopLevelPages();
+    const nav = getTopLevelPages();
 
-	return {
-		props: {
-			page,
-			nav,
-			projects,
-			posts,
-		},
-	};
+    return {
+        props: {
+            page,
+            nav,
+            projects,
+            posts,
+        },
+    };
 }
